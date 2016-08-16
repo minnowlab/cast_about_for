d6d4a1282957c5bd8909c92053a7f748d87a2d64
zky <361561797@qq.com>
2016-08-16 16:40:46 +0800

    简化减少重复的代码

--- lib/cast_about_for.rb
+++ lib/cast_about_for.rb
@@ -14,17 +14,11 @@ class ActiveRecord::Base
       raise ArgumentError, "Unknown cast_about_for key: '#{key}" unless CAST_ABOUT_FOR_KEY.include?(key)
     end
 
-    validate_keys = @options.slice(*CAST_ABOUT_FOR_KEY.take(3))
-    validate_by_stars = @options.slice(*CAST_ABOUT_FOR_KEY.last(2))
+    validate_keys = @options.slice(*CAST_ABOUT_FOR_KEY)
 
     validate_keys.each_value do |value|
+      value = value.is_a?(Array) ? value : value.keys
       value.each do |k|
-        raise ArgumentError, "Unknown column: #{k}" unless self.respond_to?(k) || self.column_names.include?(k.to_s)
-      end
-    end
-
-    validate_by_stars.each_value do |value|
-      value.keys.each do |k|
         raise ArgumentError, "Unknown column: #{k}" unless self.respond_to?(k) || self.column_names.include?(k.to_s)
       end
     end
