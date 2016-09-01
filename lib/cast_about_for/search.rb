e9cc83a69d4fe1724e9357c71ad1956ca6aee991
zky <361561797@qq.com>
2016-09-01 14:56:40 +0800

    修改条件判断

--- lib/cast_about_for/search.rb
+++ lib/cast_about_for/search.rb
@@ -23,7 +23,7 @@ module CastAboutFor
     def cast_about_for_by_equal search_values, params, seach_model
       search_values.each do |search_value|
         search_column, search_name = obtain_value(search_value)
-        seach_model = seach_model.where("#{search_column} = ?", params[search_name.to_sym]) if params[search_name.to_sym].present?
+        seach_model = seach_model.where("#{search_column} = ?", params[search_name.to_sym]) if params.present? && params[search_name.to_sym].present?
       end
       seach_model
     end
@@ -31,7 +31,7 @@ module CastAboutFor
     def cast_about_for_by_like search_values, params, seach_model
       search_values.each do |search_value|
         search_column, search_name = obtain_value(search_value)
-        seach_model = seach_model.where("#{search_column} LIKE ?", "%#{params[search_name.to_sym]}%") if params[search_name.to_sym].present?
+        seach_model = seach_model.where("#{search_column} LIKE ?", "%#{params[search_name.to_sym]}%") if params.present? && params[search_name.to_sym].present?
       end
       seach_model
     end
@@ -63,7 +63,7 @@ module CastAboutFor
     def cast_about_for_by_enum search_values, params, seach_model
       search_values.each do |search_value|
         search_column, search_name = obtain_value(search_value)
-        seach_model = seach_model.where("#{search_column} = ?", self.send(search_column.to_s.pluralize.to_sym)[params[search_name.to_sym]]) if params[search_name.to_sym].present?
+        seach_model = seach_model.where("#{search_column} = ?", self.send(search_column.to_s.pluralize.to_sym)[params[search_name.to_sym]]) if params.present? && params[search_name.to_sym].present?
       end
       seach_model
     end
@@ -77,7 +77,7 @@ module CastAboutFor
 
     def find_records_from_by_star(search_value, params, seach_model, step)
       search_column, search_name = obtain_by_star_value(search_value, params)
-      seach_model = seach_model.send("#{step}", params[search_name.to_sym].to_datetime, field: "#{self.to_s.tableize}.#{search_column.to_s}") if params[search_name.to_sym].present?
+      seach_model = seach_model.send("#{step}", params[search_name.to_sym].to_datetime, field: "#{self.to_s.tableize}.#{search_column.to_s}") if params.present? && params[search_name.to_sym].present?
       seach_model
     end
 
