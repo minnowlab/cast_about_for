8075f1eb84adf4e3f689f30876ccb693c279e564
zky <361561797@qq.com>
2016-09-01 17:40:17 +0800

    搜索的完整型的语句

--- lib/cast_about_for/search.rb
+++ lib/cast_about_for/search.rb
@@ -23,7 +23,7 @@ module CastAboutFor
     def cast_about_for_by_equal search_values, params, seach_model
       search_values.each do |search_value|
         search_column, search_name = obtain_value(search_value)
-        seach_model = seach_model.where("#{search_column} = ?", params[search_name.to_sym]) if params.present? && params[search_name.to_sym].present?
+        seach_model = seach_model.where("#{self.to_s.tableize}.#{search_column} = ?", params[search_name.to_sym]) if params.present? && params[search_name.to_sym].present?
       end
       seach_model
     end
@@ -31,7 +31,7 @@ module CastAboutFor
     def cast_about_for_by_like search_values, params, seach_model
       search_values.each do |search_value|
         search_column, search_name = obtain_value(search_value)
-        seach_model = seach_model.where("#{search_column} LIKE ?", "%#{params[search_name.to_sym]}%") if params.present? && params[search_name.to_sym].present?
+        seach_model = seach_model.where("#{self.to_s.tableize}.#{search_column} LIKE ?", "%#{params[search_name.to_sym]}%") if params.present? && params[search_name.to_sym].present?
       end
       seach_model
     end
@@ -63,7 +63,7 @@ module CastAboutFor
     def cast_about_for_by_enum search_values, params, seach_model
       search_values.each do |search_value|
         search_column, search_name = obtain_value(search_value)
-        seach_model = seach_model.where("#{search_column} = ?", self.send(search_column.to_s.pluralize.to_sym)[params[search_name.to_sym]]) if params.present? && params[search_name.to_sym].present?
+        seach_model = seach_model.where("#{self.to_s.tableize}.#{search_column} = ?", self.send(search_column.to_s.pluralize.to_sym)[params[search_name.to_sym]]) if params.present? && params[search_name.to_sym].present?
       end
       seach_model
     end
